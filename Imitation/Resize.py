812f33a9d39cf68aa1b127d957dd1d51ffe2e314
IanFehl <42170431+IanFehl@users.noreply.github.com>
2019-08-04 22:09:46 -0500

    Update Resize.py

--- Imitation/Resize.py
+++ Imitation/Resize.py
@@ -1,19 +1,25 @@
 from PIL import Image
 import os
 
-path = "C:/Users/Ian/PycharmProjects/Handwriting/Letters" # path to get images from
+path = "C:/Users/Ian/PycharmProjects/Handwriting/Letters/Gale" # path to get images from
 dirs = os.listdir(path)
 
-path2 = "C:/Users/Ian/PycharmProjects/Handwriting/Letters-Resized/" # path to save images to
+path2 = "C:/Users/Ian/PycharmProjects/Handwriting/Letters-Resized/Gale" # path to save images to
 
-img_width = 200 # width of new image
-img_height = 200 # heigth of new image
+img_width = 128 # width of new image
+img_height = 128 # heigth of new image
 dim = (img_width, img_height)
 
 for file_name in dirs: # for each file in the given directory
-  image = Image.open(os.path.join(path, file_name)) # open image
+    image = Image.open(os.path.join(path, file_name)) # open image
 
-  output = image.resize(dim, Image.ANTIALIAS) # resize image
+    output = image.resize(dim, Image.ANTIALIAS) # resize image
+    output = output.convert('LA')
+    pixels = output.load()
+    for i in range(output.size[0]):  # for every col:
+        for j in range(output.size[1]):  # For every row
+            if pixels[i, j] >= (65,255):
+                pixels[i, j] = (255, j)  # set the color accordingly
 
-  output_file_name = os.path.join(path2, "resized_" + file_name) # add "resized_" to beginning of output file name
-  output.save(output_file_name, "PNG", quality=95) # save resized image in second directory
+    output_file_name = os.path.join(path2, "resized_" + file_name) # add "resized_" to beginning of output file name
+    output.save(output_file_name, "PNG", quality=95) # save resized image in second directory
